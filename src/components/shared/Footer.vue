4fa77d110537bcc745b9b3b6256d96803e018f8b
jlyonas <jlyon@acumensolutions.com>
2018-03-05 10:57:14 -0500

    add header and footer

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,3 +1,6 @@
 <template>
-	<h1>Footer</h1>
+	<v-footer class="px-3">
+		<v-spacer></v-spacer>
+		<div>&copy; Justin Lyon {{ new Date().getFullYear() }}</div>
+	</v-footer>
 </template>
--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,3 +1,51 @@
 <template>
-	<h1>Header</h1>
+	<v-toolbar app >
+		<v-menu :nudge-width="100">
+			<v-toolbar-title slot="activator">
+				<span></span>
+				<v-icon>menu</v-icon>
+			</v-toolbar-title>
+
+			<v-list>
+				<v-list-tile v-for="link in links" :key="link.label" :href="link.href" :to="link.to" >
+					<v-list-tile-title>{{ link.label }}</v-list-tile-title>
+				</v-list-tile>
+			</v-list>
+		</v-menu>
+
+		<v-toolbar-items>
+			<v-btn flat
+				to="/"
+				exact
+				color="primary">
+				Justin Lyon
+			</v-btn>
+		</v-toolbar-items>
+
+		<v-spacer></v-spacer>
+
+		<v-toolbar-items>
+			<v-btn v-for="link in links"
+				:key="link.label"
+				:href="link.href"
+				:to="link.to"
+				flat>{{ link.label }}</v-btn>
+		</v-toolbar-items>
+
+	</v-toolbar>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			links: [
+				{ label: "Resume", to: "/resume" },
+				{ label: "LinkedIn", href: "//www.linkedin.com/in/justin-lyon" },
+				{ label: "GitHub", href: "//github.com/jlyon87" },
+			]
+		};
+	}
+}
+</script>
+
